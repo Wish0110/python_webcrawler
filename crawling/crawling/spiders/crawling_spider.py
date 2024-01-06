5f5ec27ffd0e13cf7d2022e2ccfe8260a773a98f
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-01-06 18:26:13 +0530

    return json

--- crawling/crawling/spiders/crawling_spider.py
+++ crawling/crawling/spiders/crawling_spider.py
@@ -1,5 +1,6 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+import json
 
 
 class CrawlingSpider(CrawlSpider):
@@ -12,9 +13,10 @@ class CrawlingSpider(CrawlSpider):
         Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item")
     )
 
-    def parse_item(selfself, response):
-        yield {
+    def parse_item(self, response):
+        item = {
             "title": response.css(".product_main h1::text").get(),
             "price": response.css(".price_color::text").get(),
             "availability": response.css(".availability::text")[1].get().replace("\n", "").replace(" ","")
         }
+        return json.dumps(item)
